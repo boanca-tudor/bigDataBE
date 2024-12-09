fe77ef3c13e62a876d5ed709e56dd71615b20ebf
tudor <tudorboanca@gmail.com>
2024-12-09 19:26:46 +0200

    Basic consumer-producer setup

---
+++ src/main/java/com/bigdata/app/controller/KafkaSendController.java
@@ -0,0 +1,22 @@
+package com.bigdata.app.controller;
+
+import com.bigdata.app.kafka.producer.MessageProducer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/kafka")
+public class KafkaSendController {
+
+    @Autowired
+    private MessageProducer messageProducer;
+
+    @PostMapping("/send")
+    public String sendMessage(@RequestParam("message") String message) {
+        messageProducer.sendMessage("big-data", message);
+        return "Message sent: " + message;
+    }
+}
