74b59e939ba92f16b2a1299a7bb62855a435de3d
tudor <tudorboanca@gmail.com>
2024-12-09 19:26:29 +0200

    Added spring web

--- build.gradle.kts
+++ build.gradle.kts
@@ -27,10 +27,12 @@ dependencies {
 	implementation("org.springframework.boot:spring-boot-starter-data-cassandra")
 	implementation("org.apache.kafka:kafka-streams")
 	implementation("org.springframework.kafka:spring-kafka")
+	implementation("org.springframework.boot:spring-boot-starter-web")
 	compileOnly("org.projectlombok:lombok")
 	annotationProcessor("org.projectlombok:lombok")
 	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testImplementation("org.springframework.kafka:spring-kafka-test")
+	testImplementation("org.springframework.boot:spring-boot-starter-test")
 	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
